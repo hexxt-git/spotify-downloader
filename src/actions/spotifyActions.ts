48ec40bff9f9bb540ab2fa9940f6fdd797f178ab
HEXXT <88392191+hexxt-git@users.noreply.github.com>
2025-01-01 00:55:24 +0100

    better download support

--- src/actions/spotifyActions.ts
+++ src/actions/spotifyActions.ts
@@ -22,15 +22,12 @@ export async function fetchTracks(url: string) {
 
 export async function downloadTrack(name: string, trackId: string) {
     const response = await fetch(`/api/download?id=${trackId}`);
-
     if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.error || "Failed to get download URL");
     }
 
     const data: FileResponse = await response.json();
-
-    // Initiate the download
     const downloadResponse = await fetch(data.file_url);
     if (!downloadResponse.ok) {
         throw new Error("Failed to download the file");
@@ -38,13 +35,25 @@ export async function downloadTrack(name: string, trackId: string) {
 
     if (typeof window !== "undefined") {
         const blob = await downloadResponse.blob();
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.style.display = "none";
-        a.href = url;
-        a.download = `${name}.mp3`;
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
+        if (window.navigator && (window.navigator as any).msSaveOrOpenBlob) {
+            (window.navigator as any).msSaveOrOpenBlob(blob, `${name}.mp3`);
+        } else {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.style.display = "none";
+            a.href = url;
+            a.setAttribute("download", `${name}.mp3`);
+
+            // Fallback for browsers that don't support "download"
+            if (typeof a.download === "undefined") {
+                window.open(url);
+            } else {
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            }
+
+            window.URL.revokeObjectURL(url);
+        }
     }
 }
